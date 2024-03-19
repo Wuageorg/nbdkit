4e241bd934d672c36e42d833036f06f09ca7b52e
Hugues Morisset <morisset.hugues@gmail.com>
2024-03-19 18:11:38 +0100

    T0rrent wip v2

--- plugins/golang/examples/t0rrent/go.mod
+++ plugins/golang/examples/t0rrent/go.mod
@@ -9,12 +9,10 @@ require libguestfs.org/nbdkit v1.0.0
 require github.com/anacrolix/torrent v1.54.1
 
 require (
-	bazil.org/fuse v0.0.0-20180421153158-65cc252bf669 // indirect
 	github.com/RoaringBitmap/roaring v1.2.3 // indirect
 	github.com/ajwerner/btree v0.0.0-20211221152037-f427b3e689c0 // indirect
 	github.com/alecthomas/atomic v0.1.0-alpha2 // indirect
 	github.com/anacrolix/chansync v0.3.0 // indirect
-	github.com/anacrolix/dht v0.0.0-20181129074040-b09db78595aa // indirect
 	github.com/anacrolix/dht/v2 v2.19.2-0.20221121215055-066ad8494444 // indirect
 	github.com/anacrolix/envpprof v1.3.0 // indirect
 	github.com/anacrolix/generics v0.0.0-20230816105729-c755655aee45 // indirect
@@ -27,40 +25,26 @@ require (
 	github.com/anacrolix/multiless v0.3.0 // indirect
 	github.com/anacrolix/stm v0.4.0 // indirect
 	github.com/anacrolix/sync v0.5.1 // indirect
-	github.com/anacrolix/tagflag v1.3.0 // indirect
 	github.com/anacrolix/upnp v0.1.3-0.20220123035249-922794e51c96 // indirect
 	github.com/anacrolix/utp v0.1.0 // indirect
 	github.com/bahlo/generic-list-go v0.2.0 // indirect
 	github.com/benbjohnson/immutable v0.3.0 // indirect
 	github.com/bits-and-blooms/bitset v1.2.2 // indirect
-	github.com/boltdb/bolt v1.3.1 // indirect
 	github.com/bradfitz/iter v0.0.0-20191230175014-e8f45d346db8 // indirect
 	github.com/cespare/xxhash v1.1.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815 // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/edsrzf/mmap-go v1.1.0 // indirect
-	github.com/elgatito/upnp v0.0.0-20180711183757-2f244d205f9a // indirect
-	github.com/fsnotify/fsnotify v1.5.4 // indirect
-	github.com/glycerine/go-unsnap-stream v0.0.0-20190901134440-81cf024a9e0a // indirect
 	github.com/go-llsqlite/adapter v0.0.0-20230927005056-7f5ce7f0c916 // indirect
 	github.com/go-llsqlite/crawshaw v0.4.0 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/btree v1.1.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
-	github.com/gosuri/uilive v0.0.0-20170323041506-ac356e6e42cd // indirect
-	github.com/gosuri/uiprogress v0.0.0-20170224063937-d0567a9d84a1 // indirect
 	github.com/huandu/xstrings v1.3.2 // indirect
-	github.com/ipfs/go-ipfs v0.4.18 // indirect
-	github.com/jessevdk/go-flags v1.5.0 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
-	github.com/mattn/go-sqlite3 v1.14.16 // indirect
 	github.com/mschoch/smat v0.2.0 // indirect
-	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7 // indirect
-	github.com/philhofer/fwd v1.0.0 // indirect
 	github.com/pion/datachannel v1.5.2 // indirect
 	github.com/pion/dtls/v2 v2.2.4 // indirect
 	github.com/pion/ice/v2 v2.2.6 // indirect
@@ -80,17 +64,9 @@ require (
 	github.com/pion/udp v0.1.4 // indirect
 	github.com/pion/webrtc/v3 v3.1.42 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/rs/dnscache v0.0.0-20211102005908-e0241e321417 // indirect
-	github.com/ryszard/goskiplist v0.0.0-20150312221310-2dfbae5fcf46 // indirect
-	github.com/spaolacci/murmur3 v0.0.0-20180118202830-f09979ecbc72 // indirect
-	github.com/stretchr/testify v1.8.1 // indirect
-	github.com/syncthing/syncthing v0.14.48-rc.4 // indirect
 	github.com/tidwall/btree v1.6.0 // indirect
-	github.com/tinylib/msgp v1.1.2 // indirect
-	github.com/willf/bitset v1.1.10 // indirect
-	github.com/willf/bloom v0.0.0-20170505221640-54e3b963ee16 // indirect
 	go.etcd.io/bbolt v1.3.6 // indirect
 	go.opentelemetry.io/otel v1.8.0 // indirect
 	go.opentelemetry.io/otel/trace v1.8.0 // indirect
--- plugins/golang/examples/t0rrent/t0rrent.go
+++ plugins/golang/examples/t0rrent/t0rrent.go
@@ -4,44 +4,218 @@ import (
 	"C"
 	"unsafe"
 
-	// "log"
+	"runtime"
+	"runtime/debug"
+
+	"log"
+	"io"
+	"sync"
+	"errors"
+	"strings"
 
 	"libguestfs.org/nbdkit"
 
 	"github.com/anacrolix/torrent"
-	// "github.com/anacrolix/torrent/storage"
+	"github.com/anacrolix/torrent/metainfo"
+	ts "github.com/anacrolix/torrent/storage"
 )
 
-// Future Needs
+// Needs
 // 14 local peers discovery missing
 // 54 idonthave missing
-// 52 bittorentv2 missing
+// 52 bittorentv2 missing (is on master)
+// torrent modifications
+
+// Have
 // 19 webseeds
 // 29 uTorrent transport protocol
 // 27 private torrents
 // 7 ipv6
 // 5 dht
-// torrent modifications
-// storage backend
 
-var pluginName = "t0rrent"
+
+// FIXME how to not compile anacrolix webrtc stuff
+
+// 💾💾💾💾💾💾💾💾
+// 💾💾💾💾💾💾💾💾
+// 💾💾 Storage
+// 💾💾💾💾💾💾💾💾
+// 💾💾💾💾💾💾💾💾
+
+// Storage struct
+type StorLinuxCache struct {
+	ts.ClientImpl
+
+	torrs   map[metainfo.Hash]*TorrStorLinuxCache
+	mu      sync.Mutex
+}
+
+func NewStorage() *StorLinuxCache {
+	stor := new(StorLinuxCache)
+	stor.torrs = make(map[metainfo.Hash]*TorrStorLinuxCache)
+	return stor
+}
+
+func (s *StorLinuxCache) OpenTorrent(info *metainfo.Info, infoHash metainfo.Hash) (ts.TorrentImpl, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	torr := NewTorrent(s)
+	torr.Init(info, infoHash)
+	s.torrs[infoHash] = torr
+	return ts.TorrentImpl{Piece: torr.Piece, Close: torr.Close, Flush: torr.Flush}, nil //	OE
+}
+
+func (s *StorLinuxCache) CloseHash(hash metainfo.Hash) {
+	if s.torrs == nil {
+		return
+	}
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if torr, ok := s.torrs[hash]; ok {
+		torr.Close()
+		delete(s.torrs, hash)
+	}
+
+	runtime.GC()
+	debug.FreeOSMemory()
+}
+
+func (s *StorLinuxCache) Close() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	for _, torr := range s.torrs {
+		torr.Close()
+	}
+	return nil
+}
+
+func (s *StorLinuxCache) GetCache(hash metainfo.Hash) *TorrStorLinuxCache {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if torr, ok := s.torrs[hash]; ok {
+		return torr
+	}
+	return nil
+}
+
+type TorrStorLinuxCache struct {
+	// iface ts.TorrentImpl
+	// stor *StorLinuxCache
+	Capacity ts.TorrentCapacity
+
+	hash     metainfo.Hash
+	pieceLength int64
+	pieceCount  int
+	isClosed bool
+
+	// seenPieces map[int]*Bool // TODO check kernel cache if the piece as already been seen
+}
+
+func NewTorrent(storage *StorLinuxCache) *TorrStorLinuxCache {
+	return &TorrStorLinuxCache{}
+}
+
+func (t *TorrStorLinuxCache) Init(info *metainfo.Info, hash metainfo.Hash) {
+	t.pieceLength = info.PieceLength
+	t.pieceCount = info.NumPieces()
+	t.hash = hash
+}
+
+func (t *TorrStorLinuxCache) Piece(m metainfo.Piece) ts.PieceImpl {
+	return &PieceFake{}
+	// idx := m.Index()
+	// if idx >= t.pieceCount {
+	// 	return &PieceFake{}
+	// }
+	// t.muPieces.Lock()
+	// defer t.muPieces.Unlock()
+	// if val, ok := t.inFlightPiecesMap[idx]; ok {
+	// 	return t.inFlightPieces[val]
+	// } else {
+	// 	p := NewMemPiece(i, c.pieceLength)
+	// 	append(t.inFlightPieces, p)
+	// 	t.inFlightPiecesMap[idx] = t.inFlightPieces.len - 1
+	// 	return p
+	// }
+}
+
+// func (t *TorrStorLinuxCache) AdjustRA(readahead int64) {
+// 	if t.Readers() > 0 {
+// 		t.muReaders.Lock()
+// 		for r := range t.readers {
+// 			r.SetReadahead(readahead)
+// 		}
+// 		t.muReaders.Unlock()
+// 	}
+// }
+
+func (t *TorrStorLinuxCache) Close() error {
+	t.isClosed = true
+	// t.stor.CloseHash(t.hash)
+	return nil
+}
+
+func (t *TorrStorLinuxCache) Flush() error {
+	return nil
+}
+
+type PieceFake struct{}
+
+func (PieceFake) ReadAt(p []byte, off int64) (n int, err error) {
+	err = errors.New("fake")
+	return
+}
+
+func (PieceFake) WriteAt(p []byte, off int64) (n int, err error) {
+	err = errors.New("fake")
+	return
+}
+
+func (PieceFake) MarkComplete() error {
+	return errors.New("fake")
+}
+
+func (PieceFake) MarkNotComplete() error {
+	return errors.New("fake")
+}
+
+func (PieceFake) Completion() ts.Completion {
+	return ts.Completion{
+		Complete: false,
+		Ok:       true,
+		Err:      nil,
+	}
+}
+
+// 🎛️🎛️🎛️🎛️🎛️🎛️🎛️🎛️
+// 🎛️🎛️🎛️🎛️🎛️🎛️🎛️🎛️
+// 🎛️🎛️ Plugin
+// 🎛️🎛️🎛️🎛️🎛️🎛️🎛️🎛️
+// 🎛️🎛️🎛️🎛️🎛️🎛️🎛️🎛️
 
 // The plugin global struct.
+
 type T0rrentPlugin struct {
-	nbdkit.Plugin
+	nbdkit.Plugin // iface
 	magnet string
-	client *torrent.Client
+	tManager *torrent.Client
+	t *torrent.Torrent
 }
 
 // The per-nbd-client struct.
 type T0rrentConnection struct {
-	nbdkit.Connection
-	plugin 	*T0rrentPlugin
+	nbdkit.Connection // iface
+	plugin *T0rrentPlugin
+	reader torrent.Reader
 }
 
 func (p *T0rrentPlugin) Config(key string, value string) error {
 	if key == "magnet" {
-		p.magnet = value
+		if strings.HasPrefix(value, "magnet:") {
+			p.magnet = value
+		} else {
+			p.magnet = "magnet:?xt=urn:btih:" + value
+		}
 		return nil
 	} else {
 		return nbdkit.PluginError{Errmsg: "unknown parameter"}
@@ -63,26 +237,51 @@ func (p *T0rrentPlugin) GetReady() error {
 	torrentCfg.DisableIPv4 = false
 	torrentCfg.DisableTCP = true
 	torrentCfg.DisableUTP = false
-	torrentCfg.Debug = true
-	// torrentCfg.DefaultStorage = st
+	torrentCfg.DefaultStorage = NewStorage()
+	// torrentCfg.Debug = true
 
 	var err error
-	p.client, err = torrent.NewClient(torrentCfg)
+	p.tManager, err = torrent.NewClient(torrentCfg)
 	if err != nil {
 		return err
 	}
-	p.client.AddMagnet(p.magnet);
+
+	p.t, err = p.tManager.AddMagnet(p.magnet);
+	if err != nil {
+		return err
+	}
+	p.t.SetDisplayName("Downloading torrent metadata");
+	// func (t *Torrent) AddWebSeeds(urls []string, opts ...AddWebSeedsOpt)
+	// func (t *Torrent) AddTrackers(announceList [][]string)
+	// func (t *Torrent) AddPeers(pp []PeerInfo) (n int)
+	// func (t *Torrent) CancelPieces(begin, end pieceIndex)
+	// func (t *Torrent) DownloadPieces(begin, end pieceIndex)
+	// func (t *Torrent) Length() int64
+	// func (t *Torrent) NumPieces() pieceIndex
+	// func (t *Torrent) Piece(i pieceIndex) *Piece
+	// func (t *Torrent) SubscribePieceStateChanges() *pubsub.Subscription[PieceStateChange]
+	<-p.t.GotInfo() // wait till with get torrent infos
+	log.Println("InfoHash", p.t.InfoHash())
+	log.Println("Name", p.t.Info().BestName())
+	log.Println("Piece Length", p.t.Info().PieceLength)
+	log.Println("CreationDate", p.t.Metainfo().CreationDate)
+	log.Println("CreatedBy", p.t.Metainfo().CreatedBy)
+	log.Println("Comment", p.t.Metainfo().Comment)
+	for _, f := range p.t.Files() {
+		log.Println("File", f.Path(), f.Length())
+	}
 	return nil
 }
 
 func (p *T0rrentPlugin) Open(readonly bool) (nbdkit.ConnectionInterface, error) {
 	return &T0rrentConnection{
 		plugin: p,
+		reader: p.t.NewReader(),
 	}, nil
 }
 
 func (c *T0rrentConnection) GetSize() (uint64, error) {
-	return 100, nil
+	return uint64(c.plugin.t.Length()), nil
 }
 
 // Clients are allowed to make multiple connections safely.
@@ -90,17 +289,23 @@ func (c *T0rrentConnection) CanMultiConn() (bool, error) {
 	return true, nil
 }
 
-func (c *T0rrentConnection) PRead(buf []byte, offset uint64,
-	flags uint32) error {
-	// copy(buf, disk[offset:int(offset)+len(buf)])
-	return nil
-}
-
-
-
-
 func (c *T0rrentConnection) CanWrite() (bool, error) {
 	return false, nil
+}
+
+func (c *T0rrentConnection) PRead(buf []byte, offset uint64,
+	flags uint32) error {
+	log.Println("Read Offset: ", offset, " flags: ", flags)
+
+	newPos, err := c.reader.Seek(int64(offset), io.SeekStart)
+	if err != nil {
+		return err
+	}
+	if newPos != int64(offset) {
+		log.Println("SEEK FAILED OFFSET %d SEEKTO %d", offset, newPos)
+	}
+	_, err = c.reader.Read(buf)
+	return nil
 }
 
 func (c *T0rrentConnection) PWrite(buf []byte, offset uint64,
@@ -121,7 +326,7 @@ func plugin_init() unsafe.Pointer {
 	// ...
 
 	// Then you must call the following function.
-	return nbdkit.PluginInitialize(pluginName, &T0rrentPlugin{})
+	return nbdkit.PluginInitialize("t0rrent", &T0rrentPlugin{})
 }
 
 // This is never(?) called, but must exist.
